ff28a2d8df32774eec380f001ef64b66e435cdd8
Alena Trushnikova <alena.yu.t@gmail.com>
2021-01-25 15:17:00 -0800

    migrations created

---
+++ expenses_tracker_backend/db/migrate/20210125230926_create_expenses.rb
@@ -0,0 +1,13 @@
+class CreateExpenses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :expenses do |t|
+      t.float :amount
+      t.date :date
+      t.string :description
+      t.belongs_to :user
+      t.belongs_to :category
+
+      t.timestamps
+    end
+  end
+end
