950d8d5e0afb7381ca71573e27a9decdc3d54df6
Alena Trushnikova <alena.yu.t@gmail.com>
2021-01-25 17:43:39 -0800

    controllers set up

--- expenses_tracker_backend/app/controllers/expenses_controller.rb
+++ expenses_tracker_backend/app/controllers/expenses_controller.rb
@@ -1,6 +1,34 @@
 class ExpensesController < ApplicationController
-  def index
-    expenses = User.all
-    render json: expenses
-  end
+  # def index
+  #   expenses = Expense.all
+  #   render json: expenses
+  # end
+  #
+  # def show
+  #   expense = Expense.find_by(params[:id])
+  #   render json: expense
+  # end
+  #
+  # def edit
+  #   expense = Expense.find_by(id: params[:id])
+  # end
+  #
+  # def update
+  #   expense = Expense.find_by(id: params[:id])
+  #   expense.update(date: params[:date], name: params[:name])
+  #   render json: expense
+  # end
+  #
+  # def create
+  #   expense = Expense.create(amount: params[:amount], date: params[:date], description: params[:description], user_id: params[:user_id], category_id: params[:category_id])
+  #   render json: expense
+  # end
+  #
+  # def destroy
+  #
+  #   expense = Expense.find_by(params[:id])
+  #   expense.destroy
+  #   render json: {"Message": "Expense was deleted"}
+  # end
+
 end
--- expenses_tracker_backend/app/controllers/users_controller.rb
+++ expenses_tracker_backend/app/controllers/users_controller.rb
@@ -4,4 +4,25 @@ class UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+  def show
+    user = User.find_by(params[:id])
+    render json: user
+  end
+
+  # def create
+  #   user = User.find_or_create_by(name: params[:name], budget: params[:budget])
+  #   render json: user
+  # end
+  #
+  # def update
+  #   user = User.update(name: params[:name], budget: params[:budget])
+  #   render json: user
+  # end
+  #
+  # def destroy
+  #   user = User.find_by(id: params[:id])
+  #   user.destroy
+  #   render json: { "Message": "User was deleted" }
+  # end
 end
